af72b92d48fdc27855e3a910c82ab76949611102
Admin <Admin@TEB123004>
2023-10-06 14:34:57 +0200

    Add project files.

---
+++ Uppgift3_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket land vann fotbolls-VM för damer år 2015?");
+            string svar = Console.ReadLine().ToLower();
+
+            if(svar == "usa")
+            {
+                Console.WriteLine("Helt Rätt!");
+            }
+            if (svar != "usa") 
+            {
+                Console.WriteLine("Fel Svar!");
+            }
+        }
+    }
+}
